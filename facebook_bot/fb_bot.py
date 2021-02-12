55bb9427dbcbc104cf5e117987cb98c248d083bd
PranavDuggal <70065423+PranavDuggal@users.noreply.github.com>
2021-02-12 20:06:02 +0530

    Write post on friend profile
    Test user changed.
    Test user can access friend's profile and write a comment.

--- facebook_bot/fb_bot.py
+++ facebook_bot/fb_bot.py
@@ -2,6 +2,8 @@ import time
 import pydirectinput
 from numpy import random
 
+from selenium.common.exceptions import *
+
 class Facebook_Bot:
 
         def __init__(self,driver,config,profile_friends):
@@ -30,17 +32,27 @@ class Facebook_Bot:
                 self.driver.get(url)
                 time.sleep(6)
 
-        def add_friend(self,friend_name="M"):
+        def add_friend(self,friend_name="Ullrich Alfceiccdichh Sadanstein"):
                 # Possible exceptions - Name doesn't exist; Already added as a friend
+                
                 time.sleep(6)
                 search_bar = self.driver.find_element_by_xpath(self.search_bar_xpath)
                 search_bar.click()
                 search_bar.send_keys(friend_name)
-                pydirectinput.moveTo(100, 150) 
-                pydirectinput.press('enter')
+                pydirectinput.moveTo(100, 150)
+                time.sleep(1)
+                pydirectinput.moveTo(120,250) 
+                pydirectinput.click()
+                
                 time.sleep(6)
-                pydirectinput.moveTo(1568,390)
+                add_friend = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[2]/div/div/div/div[1]/div/div')
+                add_friend.click()
                 time.sleep(6)
+                cancel_request = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[2]/div/div/div/div[1]/div/div')
+                cancel_request.click()
+                time.sleep(6)
+                
+
 
         def write_post(self,message = "I am a Bot",url="https://www.facebook.com"):
                 # Possible exceptions: If posting same as previous in short time, throws an exception
@@ -61,14 +73,22 @@ class Facebook_Bot:
                 time.sleep(6)
                 self.driver.get(url)
                 time.sleep(6)
-                x = random.choice(range(1,3))
+                x = random.choice(range(1,9))
                 friend = self.profile_friends[x]
                 friend_comment = self.driver.find_element_by_xpath(friend)
                 friend_comment.click()
                 time.sleep(6)
 
-        # def comment_on_friends_post(self):
-        #         # Possible Exceptions - No post on profile
-
-        # def post_on_friends_profile(self):
-        #         # wfl
+        def post_on_friends_profile(self,message):
+                # Possible Exceptions - No post on profile
+                self.open_friends_profile_randomly()
+                time.sleep(6)
+                post = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]/div/div[1]/span')
+                post.click()
+                time.sleep(6)
+                post_text = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div[2]/div[1]/div/div[1]/div[1]/div/div/div/div/div[2]/div/div/div/div')
+                post_text.send_keys(message)
+                time.sleep(6)
+                post_button = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div[3]/div[2]/div/div[1]')
+                post_button.click()
+                time.sleep(6)
--- main.py
+++ main.py
@@ -14,34 +14,51 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from numpy import random
+
+# # Importing package to output random strings
+from essential_generators import DocumentGenerator
+gen = DocumentGenerator()
+
+# Import X Paths of friend grid on profile page
 from friends import friends
 
+# Import the class
 from facebook_bot.fb_bot import Facebook_Bot
 
+# Import user details stored as json
 with open('config.json') as file:
         config = json.load(file)
 
-# Import class from facebook.py file
-
-
+# Setting up chrome driver
 options=webdriver.ChromeOptions()
 options.headless=False
 prefs={"profile.default_content_setting_values.notifications", 2}
-driver = webdriver.Chrome(executable_path="C:/Users/prana/OneDrive/Documents/chromedriver.exe",chrome_options=options)
+driver = webdriver.Chrome(executable_path="Driver\chromedriver.exe",chrome_options=options)
 
+##############################################################################################
+# Facebook Bot Flow
+# 1. Login
+# 2. Open account profile page
+# 3. Add a friend
+# 4. Write a post from home page
+# 5. Open a random friends profile and write a post
 
 bot = Facebook_Bot(driver,config,friends)
 
 
-bot.login()
-bot.open_profile()
-
-from essential_generators import DocumentGenerator
-gen = DocumentGenerator()
-bot.write_post(gen.sentence())
+bot.login() # Logs into the facebook account
 
 
-bot.open_friends_profile_randomly()
+bot.open_profile() # Opens the Profile Page
 
+
+bot.add_friend() # This Function helps to add a Friend
+
+
+bot.write_post(gen.sentence()) # This Helps in posting a message on your profile
+
+
+# bot.open_friends_profile_randomly() # This functions Randomly chooses a friend in your friends list
+bot.post_on_friends_profile(gen.sentence()) # This opens a friends profile randomly and writes a post
 
 
